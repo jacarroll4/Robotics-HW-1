b3bf6500ffe046c42865a14f36f76eeecedb58f0
jacarroll4 <jacarroll4@crimson.ua.edu>
2025-09-17 19:50:05 -0500

    Update diamond.py
    comment diamond

--- diamond.py
+++ diamond.py
@@ -18,23 +18,23 @@ class TurtleController(Node):
 
     def get_twist_msg(self):
         if self.time < 1:
-            msg = self.create_twist(0.0,1.571)
+            msg = self.create_twist(0.0,1.571) # rotate 45 degrees to make diamond clearer to see
         elif self.time >= 1 and self.time < 3:
-            msg = self.create_twist(1.0, 0.0)
+            msg = self.create_twist(1.0, 0.0) # make first line of diamond
         elif self.time >= 3 and self.time < 4:
-            msg = self.create_twist(0.0, 2.09)
+            msg = self.create_twist(0.0, 2.09) # rotate 120 degrees roughly
         elif self.time >= 4 and self.time < 6:
-            msg = self.create_twist(1.0, 0.0)
+            msg = self.create_twist(1.0, 0.0) # make second, equal-sized line of diamond
         elif self.time >= 6 and self.time < 7:
-            msg = self.create_twist(0.0, 4.101)
+            msg = self.create_twist(0.0, 4.101) # rotate roughly 235 degrees
         elif self.time >= 7 and self.time < 9:
-            msg = self.create_twist(1.0, 0.0)
+            msg = self.create_twist(1.0, 0.0) # make third equal sized line of diamond
         elif self.time >= 9  and self.time < 10:
-            msg = self.create_twist(0.0, 2.09)
+            msg = self.create_twist(0.0, 2.09) # rotate same 120 degrees roughly to complete 3/4 of diamond
         elif self.time >= 10 and self.time < 12:
-            msg = self.create_twist(1.0, 0.0)
+            msg = self.create_twist(1.0, 0.0) # draw final line to make diamond 4 equals sides and angles
         else:
-            msg = self.create_twist(0.0, 0.0)
+            msg = self.create_twist(0.0, 0.0) # stop moving once shape is drawn
         return msg
     
     def timer_callback(self):
